e1192d50597755f5ef96e642683af02106fea17f
Camila Laruccia <camilalaruccia@gmail.com>
2023-04-05 14:26:22 -0300

    Revisão e separação do JS

---
+++ js/01-sintaxe.js
@@ -0,0 +1,49 @@
+// CTRL:
+
+/* SHIFT ALT A */
+
+/* variante e constante
+recursos para armazenamento e manipulaçaõ de dados na memoria RAM.
+let-indica variavel
+const-indica a constante*/
+
+// Criando variaveis
+let ano = 2023;
+let curso = "Programador de web";
+let cidade = "São Paulo";
+
+// Criando constantes
+const nome = "Camila";
+const escola = "Senac";
+
+// Saida de dados (no console)
+console.log("exemplos");
+console.log(curso);
+console.log(ano);
+console.log(escola);
+
+let produto = "TVLed";
+let preco = 1500;
+quantidade = 3;
+total = preco *quantidade;
+
+console.log(produto);
+console.log(preco, quantidade);
+console.log(nome);
+
+/* Função tofixed(número) Usada para determinar quantidade de casas decimais */
+console.log( total.toFixed(2));
+
+
+/* Sa´da de dados estátidos e dinâmicos */
+
+// Forma 1 (moderna): Template String/literal
+console.log(`Olá!Me chamo ${nome}  estou fazendo o curso de ${curso} no ${escola}`);
+
+
+
+// Forma 2 (antiga/tradicional): concatenação
+console.log("Olá! Me chamo " + nome + " estou fazendo o curso de " + curso + " no "+escola);
+
+
+
