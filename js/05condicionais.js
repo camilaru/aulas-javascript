9bffaa5012f8ec1f1458f3107f50e85a59100970
Camila Laruccia <camilalaruccia@gmail.com>
2023-04-06 16:25:23 -0300

    condicional encadeada

--- js/05condicionais.js
+++ js/05condicionais.js
@@ -30,11 +30,37 @@ const limiteDeFaltas = 10;
 
 /* Se a média obtida pelo aluno(a) for maior ou igual a media minima da escola, então ele estará aprovado. Caso contrário ele estara reprovado */
 
-if (media >= mediaMinima && faltas < limiteDeFaltas){
-console.log("aprovado"); 
+if (media >= mediaMinima && faltas < limiteDeFaltas) {
+    console.log("aprovado");
 } else {
     console.log("reprovado");
 }
+
+/* Condicional Encadeada */
+
+/* Algoritmo para Análise de desempenho
+Média menor que 3? PÈSSIMO
+Média menor que 5? RUIM
+Média menor que 7? Regular
+Média menor que 9? Bom
+Nenhuma das anteriores? Òtimo */
+
+//Variavel que irá armazenar o texto/desempenho
+
+let desempenho; //começa indefinida
+if (media < 3) {
+    desempenho = "PÉSSIMO";
+} else if (media < 5 ){
+    desempenho = "RUIM";
+} else if (media < 7) {
+    desempenho = "REGULAR";
+} else if (media < 9) {
+    desempenho = "BOM";
+} else {
+    desempenho = "ÓTIMO";
+}
+
+console.log(desempenho);
 
 
 
