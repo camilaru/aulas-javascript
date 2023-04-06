a4fe9ae5a1bc36ddc3e0d5d84f654cf6ddc3a67e
Camila Laruccia <camilalaruccia@gmail.com>
2023-04-06 14:48:21 -0300

    exercício objetos
    Criando listas de produtos

--- js/04-objetos.js
+++ js/04-objetos.js
@@ -7,7 +7,7 @@ como propriedades ou atributos. */
 //Exemplo 1:Objeto simples
 let livro = {
     // chave: valor
-    titulo : "Senhor dos Anéis", //propriedade
+    titulo: "Senhor dos Anéis", //propriedade
     ano: 1954,
     volume: 3,
     autor: "J.R.R.Tolkien"
@@ -61,7 +61,40 @@ let livros = [
 ];
 
 console.log(livros[1].titulo); //O Hobbit
-console.log(livros[0].autor);  //Nick Manson
+console.log(livros[0].autor); //Nick Manson
+
+/* Exercícios
+1) Crie um objeto chamado "pedido" 
+contendo as seguimtes informações:
+
+-Código (invente uma combinação de letras e números)
+-lista de 3 produtos (basta o nome de cada um)
+-Data do pedido (coloque o dia de hoje)
+-Preço
+-Prazo de entrega em dias
+
+2) Mostre no console uma frase contendo algumas informações
+ do pedido 123xyz os produtos foram comprados em 06/04/23
+ custando 4500. O prazo de entrega é de 7 dias úteis.*/
+
+console.log("-----------");
+
+let pedido =
+{
+    codigo: "cod01355001",
+    produtos: ["tv", "ventilador", "smartphone"],
+    data: "06/04/23",
+    preco: 4500,
+    prazo: "7 dias úteis",
+}
+
+let configuracoes = {
+    style: "currency",
+    currency: "BRL"
+}
+
+
+console.log(`Referente ao pedido ${pedido.codigo} os produtos foram comprados em ${pedido.data} custando ${pedido.preco.toLocaleString("pt-br", configuracoes)}.O prazo de entrega é de ${pedido.prazo}.`)
 
 
 
