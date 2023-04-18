25d1cc135269c825e66874cfe37faffee07cb7dc
Camila Laruccia <camilalaruccia@gmail.com>
2023-04-18 17:28:28 -0300

    exercicio advinhação

---
+++ js/exercicio3.js
@@ -0,0 +1,11 @@
+console.log("Exercício 03");
+
+
+ Gerar valores entre0 e 10(sem casa decimal)
+let numeroAleatorio = parseInt(Math.random() * 11);
+console.log(numeroAleatorio);
+
+const form = document.querySelector("form");
+const palpite = document.querySelector("#palpite");
+const resultado = document.querySelector("#resultado");
+
